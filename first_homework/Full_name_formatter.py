e39d9bde0e4354b536167e43a69999e8ff82db11
Paulo <2530384@upv.mx>
2025-11-25 17:23:10 -0600

    AVANCE

---
+++ first_homework/Full_name_formatter.py
@@ -0,0 +1,9 @@
+# PROBLEM 1: FULL NAME FORMATTER (NAME+ INITIALS)
+"""
+    Descripcion: 
+    Este modulo contiene una funcion que formatea nombres completos dados en una sola cadena.
+    La funcion toma un nombre completo como entrada y devuelve el nombre 
+    solo que con las iniciales 
+"""
+
+
